bbdee6690adaf75893807809f7ec9ca53155a8c9
idsc3101 <idsc3101@gmail.com>
2014-10-01 17:25:34 -0500

    Solved

--- FirstNameFunc/FirstNameFunc/Form1.vb
+++ FirstNameFunc/FirstNameFunc/Form1.vb
@@ -9,7 +9,13 @@ Public Class Form1
     End Sub
 
     Function FirstName(ByVal name As String) As String
-        Return "Not Implemented Yet"
+        Dim ind1 As Integer
+        ind1 = name.IndexOf(" ")
+
+        Dim fstname As String
+        fstname = name.Substring(0, ind1)
+
+        Return fstname
     End Function
 
 End Class
